b4b8c037a1f70069a7507ce434d5afa138a3900d
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-01-28 21:19:12 +0200

    More styles and some formatting.

--- src/app.js
+++ src/app.js
@@ -132,7 +132,7 @@ export default class App extends Component {
       const end = this.state.segmentEnds[index]
       return (
         <div className="segment" key={counter}>
-          {counter}. {start}-{end}
+          <strong>{counter}.</strong> {start.toFixed(1)} ➜ {end.toFixed(1)}
           <button className="delete-segment-button" onClick={() => {
             this.deleteSegment(index)
           }}>✖
