1b9068fbc5000da2c199d82d44cff10eadce48c1
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-01-28 21:02:02 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { app, BrowserWindow } from 'electron';
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-import { enableLiveReload } from 'electron-compile';
+import {app, BrowserWindow, globalShortcut} from 'electron';
+import installExtension, {REACT_DEVELOPER_TOOLS} from 'electron-devtools-installer';
+import {enableLiveReload} from 'electron-compile';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,13 +8,13 @@ let mainWindow;
 
 const isDevMode = process.execPath.match(/[\\/]electron/);
 
-if (isDevMode) enableLiveReload({ strategy: 'react-hmr' });
+if (isDevMode) enableLiveReload({strategy: 'react-hmr'});
 
 const createWindow = async () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 600,
+    width: 1280,
+    height: 1000,
   });
 
   // and load the index.html of the app.
@@ -24,6 +24,9 @@ const createWindow = async () => {
   if (isDevMode) {
     await installExtension(REACT_DEVELOPER_TOOLS);
     mainWindow.webContents.openDevTools();
+    globalShortcut.register('CommandOrControl+R', () => {
+      mainWindow.reload();
+    })
   }
 
   // Emitted when the window is closed.
